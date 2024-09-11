d733acaf81c786078d9a4fc4ab848a8778038462
Alexander Kurz <alexander.kurz@igd.fraunhofer.de>
2024-09-11 15:30:00 +0200

    Update compute_quality

--- python/compute_quality.py
+++ python/compute_quality.py
@@ -11,21 +11,12 @@ from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
 from tqdm import tqdm
 
-from impl_pytorch.cr_fiqa.backbones.iresnet import iresnet50 as crfiqa_iresnet50
-from impl_pytorch.cr_fiqa.backbones.iresnet import iresnet100 as crfiqa_iresnet100
-from impl_pytorch.magface.network_inf import builder_inf as magface_builder_inf
-from impl_pytorch.sdd_fiqa.generate_pseudo_labels.extract_embedding.model.model import (
-    R50 as sddfiqa_r50,
-)
-from impl_pytorch.ser_fiq.backbones.iresnet import iresnet18 as serfiq_iresnet18
-from impl_pytorch.ser_fiq.backbones.iresnet import iresnet50 as serfiq_iresnet50
+from cr_fiqa.backbones.iresnet import iresnet50 as crfiqa_iresnet50  # isort: skip
+from cr_fiqa.backbones.iresnet import iresnet100 as crfiqa_iresnet100  # isort: skip
 
 AVAIL_METHODS = [
-    "sdd-fiqa",
     "crfiqa-s",
     "crfiqa-l",
-    "magface",
-    "ser-fiq",
 ]
 
 
@@ -99,25 +90,11 @@ def main(
 
     # Load model
     print(f"Loading model for {method_name}..")
-    if method_name == "sdd-fiqa":
-        model = sddfiqa_r50([112, 112], use_type="Qua")
-        model_dict = model.state_dict()
-        data_dict = {
-            key.replace("module.", ""): value for key, value in torch.load(checkpoint_fp).items()
-        }
-        model_dict.update(data_dict)
-        model.load_state_dict(model_dict)
-    elif "crfiqa" in method_name:
+    if "crfiqa" in method_name:
         if method_name == "crfiqa-s":
             model = crfiqa_iresnet50(num_features=512, qs=1, use_se=False)
         if method_name == "crfiqa-l":
             model = crfiqa_iresnet100(num_features=512, qs=1, use_se=False)
-        weight = torch.load(checkpoint_fp)
-        model.load_state_dict(weight)
-    elif method_name == "magface":
-        model = magface_builder_inf(arch="iresnet100", emb_size=512, checkpoint_fp=checkpoint_fp)
-    elif method_name == "ser-fiq":
-        model = serfiq_iresnet18(dropout=0.4, num_features=512, use_se=False)
         weight = torch.load(checkpoint_fp)
         model.load_state_dict(weight)
     else:
@@ -133,16 +110,8 @@ def main(
     for batch_data in tqdm(dataloader):
         imgs, filenames = batch_data
         with torch.no_grad():
-            if method_name == "sdd-fiqa":
-                qs = model(imgs.to(device))
-            elif "crfiqa" in method_name:
+            if "crfiqa" in method_name:
                 feats, qs = model(imgs.to(device))
-            elif method_name == "magface":
-                feats = model(imgs.to(device))
-                # Use magnitude of the features as quality score
-                qs = torch.linalg.norm(feats, axis=1)
-            elif method_name == "ser-fiq":
-                qs = model.calculate_serfiq(imgs.to(device), T=10, scaling=5.0)
             else:
                 raise NotImplementedError()
         qs_scores = qs.cpu().numpy()  # Move qs_scores to CPU and convert to numpy array
