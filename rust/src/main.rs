d5caba2f47127272d7ce7dea16526ff5a5ccfcaa
Alexander Kurz <alexander.kurz@igd.fraunhofer.de>
2025-03-19 15:34:08 +0100

    Perform model inference using rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,16 +1,41 @@
-use image::GenericImageView;
+use image::{GenericImageView, imageops::FilterType};
+use ndarray::Array4;
+use ort::session::{Session, builder::GraphOptimizationLevel};
 use show_image::{ImageInfo, ImageView, create_window, event};
 
 #[show_image::main]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Load the image
-    let img = image::open("../data/lfw-deepfunneled/Aaron_Eckhart/Aaron_Eckhart_0001.jpg")?;
-    let (width, height) = img.dimensions();
+    let original_img =
+        image::open("../data/lfw-deepfunneled/Aaron_Eckhart/Aaron_Eckhart_0001.jpg")?;
+    let (width, height) = original_img.dimensions();
     println!("height: {height}, width: {width}");
 
     // Convert to raw RGBA8 format
-    let img = img.into_rgba8();
+    let img = original_img.clone().into_rgba8();
     let pixels = img.into_raw();
+
+    // Convert model input
+    let input_img = original_img.resize_exact(112, 112, FilterType::Nearest);
+    let rgb_img = input_img.to_rgb8();
+    let img_array: Vec<f32> = rgb_img
+        .pixels()
+        .flat_map(|p| p.0.iter().map(|&v| v as f32 / 255.0)) // Normalize
+        .collect();
+    // Convert to ndarray tensor shape (1, 3, 112, 112)
+    let input_tensor = Array4::from_shape_vec((1, 3, 112, 112), img_array)?;
+
+    // Perform inference
+    let model = Session::builder()?
+        .with_optimization_level(GraphOptimizationLevel::Level3)?
+        .with_intra_threads(4)?
+        .commit_from_file("../checkpoints/onnx/crfiqa-s.onnx")?;
+
+    let outputs = model.run(ort::inputs!["input" => input_tensor]?)?;
+    let predictions = outputs["qs"].try_extract_tensor::<f32>()?;
+
+    // TODO: Extract float from output tensor
+    println!("{predictions}");
 
     // Equivalent to cv::imshow()
     let image = ImageView::new(ImageInfo::rgba8(width, height), &pixels);
