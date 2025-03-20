fbd6dad9cef2c6f129f40bc40cb8e099feb64772
Alexander Kurz <alexander.kurz@igd.fraunhofer.de>
2025-03-20 16:02:28 +0100

    Add test to test model output

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,36 +3,21 @@ use ndarray::Array4;
 use ort::session::{Session, builder::GraphOptimizationLevel};
 // use show_image::{ImageInfo, ImageView, create_window, event};
 use std::env;
+use std::error::Error;
 
 // #[show_image::main]
-pub fn run(config: Config) -> Result<(), Box<dyn std::error::Error>> {
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // Load the image
-    let original_img = image::open(config.image_p)?;
-    let (width, height) = original_img.dimensions();
-    println!("height: {height}, width: {width}");
+    // let original_img = image::open(config.image_p)?;
+    // let (width, height) = original_img.dimensions();
+    // println!("height: {height}, width: {width}");
 
     // Convert to raw RGBA8 format
     // let img_u8 = original_img.to_rgba8().into_raw();
 
-    // Convert model input
-    let img_resized = original_img.resize_exact(112, 112, FilterType::Nearest);
-    let img_f32 = img_resized.to_rgb32f().into_raw();
-    // Convert to ndarray tensor shape (1, 3, 112, 112)
-    let input_tensor = Array4::from_shape_vec((1, 3, 112, 112), img_f32)?;
-
-    // Perform inference
-    let model = Session::builder()?
-        .with_optimization_level(GraphOptimizationLevel::Level3)?
-        .with_intra_threads(4)?
-        .commit_from_file(config.model_p)?;
-
-    let outputs = model.run(ort::inputs!["input" => input_tensor]?)?;
-    let predictions = outputs["qs"].try_extract_tensor::<f32>()?;
-
-    // Extract float from output tensor
-    // println!("{predictions}");
-    // println!("Array shape: {:?}", predictions.shape());
-    for entry in predictions.iter() {
+    // Perform model inference
+    let result = model_inference(&config.model_p, &config.image_p).unwrap(); // Panics if Err
+    for entry in result {
         println!("qs: {:.4}", entry);
     }
 
@@ -76,5 +61,53 @@ impl Config {
         };
 
         Ok(Config { model_p, image_p })
+    }
+}
+
+pub fn model_inference(model_p: &str, image_p: &str) -> Result<Vec<f32>, Box<dyn Error>> {
+    let mut result = Vec::new();
+
+    // Load the image
+    let original_img = image::open(image_p)?;
+    let (width, height) = original_img.dimensions();
+    println!("height: {height}, width: {width}");
+
+    // Convert model input
+    let img_resized = original_img.resize_exact(112, 112, FilterType::Nearest);
+    let img_f32 = img_resized.to_rgb32f().into_raw();
+    // Convert to ndarray tensor shape (1, 3, 112, 112)
+    let input_tensor = Array4::from_shape_vec((1, 3, 112, 112), img_f32)?;
+
+    // Perform inference
+    let model = Session::builder()?
+        .with_optimization_level(GraphOptimizationLevel::Level3)?
+        .with_intra_threads(4)?
+        .commit_from_file(model_p)?;
+
+    let outputs = model.run(ort::inputs!["input" => input_tensor]?)?;
+    let predictions = outputs["qs"].try_extract_tensor::<f32>()?;
+
+    // Extract float from output tensor
+    // println!("{predictions}");
+    // println!("Array shape: {:?}", predictions.shape());
+    for entry in predictions.iter() {
+        result.push(*entry);
+    }
+
+    Ok(result)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use approx::assert_abs_diff_eq;
+
+    #[test]
+    fn crfiqa_s() {
+        let model_p = "../checkpoints/onnx/crfiqa-s.onnx";
+        let image_p = "../data/lfw-deepfunneled/Aaron_Eckhart/Aaron_Eckhart_0001.jpg";
+
+        let result = model_inference(model_p, image_p).unwrap(); // Panics if Err
+        assert_abs_diff_eq!(vec![1.4946].as_slice(), result.as_slice(), epsilon = 1e-4);
     }
 }
