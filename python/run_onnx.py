3fa36d515fbd8ad43b41ef3b41deb9eecf861443
Alexander Kurz <alex.kurz95@gmail.com>
2025-08-11 14:33:04 +0200

    Perform model inference with onnxruntime

--- python/run_onnx.py
+++ python/run_onnx.py
@@ -4,7 +4,9 @@ from pathlib import Path
 import numpy as np
 import onnxruntime as ort
 import pandas as pd
+import torch
 from PIL import Image
+from torchvision import transforms
 from tqdm import tqdm
 
 
@@ -25,60 +27,56 @@ def main(
     # Glob data_dir
     img_list = sorted(list(data_dir.rglob(f"*{file_ext}")))
 
-    # Read model and set input_size
+    # Load model with ort
     print("Loading onnx model..")
-    # TODO: Load model with ort
-    # model = cv2.dnn.readNetFromONNX(checkpoint_fp)
-    input_size = (112, 112)
-    # output_layer_names = model.getUnconnectedOutLayersNames()
+    session = ort.InferenceSession(checkpoint_fp)
+    output_nodes = session.get_outputs()
 
     filename_list = []
     qs_scores_arr = np.zeros(len(img_list))
-    # for idx in tqdm(range(len(img_list))):
-    idx = 0
-    img_p = img_list[idx]
-    filename = img_p.relative_to(data_dir)
-    filename_list.append(filename)
-    # Prepare model input
-    img = Image.open(img_p)
-    img = np.array(img)
-    print(img.shape)
-    # Convert (H,W,C) to (C,H,W)
-    img = img.transpose((2, 0, 1))
-    print(img.shape)
+    for idx in tqdm(range(len(img_list))):
+        img_p = img_list[idx]
+        filename = img_p.relative_to(data_dir)
+        filename_list.append(filename)
 
-    # TODO: Perform inference with ort
-    # Make blobFromImage equivalent to transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
-    # blob = cv2.dnn.blobFromImage(
-    #     img,
-    #     scalefactor=1 / (127.5),
-    #     size=input_size,
-    #     mean=(127.5, 127.5, 127.5),
-    #     swapRB=True,
-    #     crop=False,
-    # )
-    # # Run forward pass
-    # model.setInput(blob)
-    # feats, qs = model.forward(output_layer_names)
-    # qs_scores_arr[idx] = qs.item()
+        # Prepare model input
+        img = Image.open(img_p)
+        tfms = transforms.Compose(
+            [
+                transforms.Resize((112, 112)),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+            ]
+        )
+        img: torch.Tensor = tfms(img)
+        img = img.unsqueeze(0).numpy()
 
-    # # if idx == 199:
-    # #     break
+        # Run inference
+        outputs = session.run(
+            output_names=["qs"],
+            input_feed={"input": img},
+        )
+        # print(outputs[0].item())
 
-    # print("Saving results..")
-    # qs_scores_arr = qs_scores_arr[: idx + 1]
-    # print("Shape of qs_scores_arr:", qs_scores_arr.shape)
-    # filename_arr = np.array(filename_list)
+        qs_scores_arr[idx] = outputs[0].item()
 
-    # df_out = pd.DataFrame({"sample": filename_arr, "quality": qs_scores_arr})
-    # print(df_out.head())
-    # df_out.to_csv(
-    #     save_dir / f"{method_name}_{data_name}_onnx.txt",
-    #     float_format="%.6f",
-    #     header=True,
-    #     index=False,
-    #     sep=" ",
-    # )
+        if idx == 199:
+            break
+
+    print("Saving results..")
+    qs_scores_arr = qs_scores_arr[: idx + 1]
+    print("Shape of qs_scores_arr:", qs_scores_arr.shape)
+    filename_arr = np.array(filename_list)
+
+    df_out = pd.DataFrame({"sample": filename_arr, "quality": qs_scores_arr})
+    print(df_out.head())
+    df_out.to_csv(
+        save_dir / f"{method_name}_{data_name}_onnx.txt",
+        float_format="%.6f",
+        header=True,
+        index=False,
+        sep=" ",
+    )
 
 
 if __name__ == "__main__":
